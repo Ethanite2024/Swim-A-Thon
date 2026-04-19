2608417a7ac7001ef7622380227af1ab00b25232
Ethan <Ethan.sisbarro@gmail.com>
2026-04-19 11:12:57 -0400

    Added in Some of the code for a remote API

--- Swim-A-Thon/RemoteAPI.swift
+++ Swim-A-Thon/RemoteAPI.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import SwiftData
+import SwiftUI
+class RemoteAPI {
+    @AppStorage("remoteAPI") static var remoteAPI: URL = URL(string: "https://swimathon-api.placeholder.com/")!
+    func getRemoteAPI() -> URL {
+        return Self.remoteAPI
+    }
+}
