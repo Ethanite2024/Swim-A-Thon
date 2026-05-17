e1a948756d2efaf12a7afda336955cb0ae96651a
Ethan Sisbarro <Ethansisbarro@gmail.com>
2026-05-17 11:53:52 -0400

    Disabled numberToLetters and getColumns()

---
+++ Swim-A-Thon/UI and Views/LaneSelecter.swift
@@ -0,0 +1,18 @@
+//
+//  LaneSelecter.swift
+//  Swim-A-Thon
+//
+//  Created by Ethan Sisbarro on 5/17/26.
+//
+
+import SwiftUI
+
+struct LaneSelecter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaneSelecter()
+}
